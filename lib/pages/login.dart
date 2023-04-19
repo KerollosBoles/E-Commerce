bdd394e2bcc31325ea0c30f62824660851d06448
Kerollos Boles <boleskoko9@gmail.com>
2023-04-19 05:33:53 +0200

    put the text data inside the center

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -3,13 +3,15 @@ import 'package:flutter/material.dart';
 class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Text("data"),
-        ],
+      body: Center(
+        child: Column(
+          children: [
+            Text("data"),
+          ],
+        ),
       ),
-    )
+    );
   }
 }
