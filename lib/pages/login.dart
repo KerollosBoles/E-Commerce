e9d1ac6fd0e116fe7bf1451b851dc3f1365bb541
Kerollos Boles <boleskoko9@gmail.com>
2023-04-23 07:30:23 +0200

    putting the scaffold inside of safe area

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -7,63 +7,66 @@ class Login extends StatelessWidget {
   const Login({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(33.3)
-          child: Column(
-          mainAxisAlignment: MainAxisAlignmentNone.Center,
-            children: [
-              const SizedBox(
-                height: 64,
-              ),
-              MyTextField(
-              TextInputTypeee: TextInputTypeee.emailAddress,
-              isPassword: false,
-              hinttexttt: "Enter your Email Address or Username",
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 247, 247, 247),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(33.3)
+            child: Column(
+            mainAxisAlignment: MainAxisAlignmentNone.Center,
+              children: [
+                const SizedBox(
+                  height: 64,
                 ),
-              const  SizedBox(
-                height: 33
-                ),
-              MyTextField(
-              TextInputTypeee: TextInputTypeee.Password,
-              isPassword: true,,
-              hinttexttt: "Enter your password",
-              ),
-
-              const  SizedBox(
-                height: 33
+                MyTextField(
+                TextInputTypeee: TextInputTypeee.emailAddress,
+                isPassword: false,
+                hinttexttt: "Enter your Email Address or Username",
                   ),
-
-               ElevatedButton(
-                onPressed: (){}, 
-               child: Text(
-                "Sign in",
-                style: TextStyle(
-                  fontSize: 19),),
-               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(BTNgreen),
-                padding: MaterialStateProperty.all(EdgeInsets.all(zero)),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius),
-               ),
-               )
-              const  SizedBox(height: 33),
-              Row(
-                mainAxisAlignment: MainAxisAlignmentNone.Center,
-                children: [
-                    Text("Dont have an account?",
-                    style: TextStyle(fontSize: 20)),
-                  TextButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Sign up',
-                      style: TextStyle(color: Colors.black,fontSize: 20)),
+                const  SizedBox(
+                  height: 33
+                  ),
+                MyTextField(
+                TextInputTypeee: TextInputTypeee.Password,
+                isPassword: true,,
+                hinttexttt: "Enter your password",
+                ),
+    
+                const  SizedBox(
+                  height: 33
+                    ),
+    
+                 ElevatedButton(
+                  onPressed: (){}, 
+                 child: Text(
+                  "Sign in",
+                  style: TextStyle(
+                    fontSize: 19),),
+                 style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(BTNgreen),
+                  padding: MaterialStateProperty.all(EdgeInsets.all(zero)),
+                  shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius),
+                 ),
+                 )
+                const  SizedBox(height: 33),
+                Row(
+                  mainAxisAlignment: MainAxisAlignmentNone.Center,
+                  children: [
+                      Text("Dont have an account?",
+                      style: TextStyle(fontSize: 18)),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(color: Colors.black,fontSize: 18)),
+                    )
+    
+                  ],
                   )
-
-                ],
-                )
-
-            ],
+    
+              ],
+            ),
           ),
         ),
       ),
