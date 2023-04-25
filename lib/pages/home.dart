ac73c50a1cf5568c0486c2031897a83437724935
Kerollos Boles <boleskoko9@gmail.com>
2023-04-25 05:39:06 +0200

    putting the price for every image

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,27 +9,56 @@ class Home extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
 
-        body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount:2,
-          childAspectRatio: 3/2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 33,),
-          itemCount: 4,
-          itemBuilder: (BuildContext context,int index){
-            return GridTile(
-                   child: GestureDetector(
-                onTap: () { },
-                child: Image.asset("img PATH")
-         
+        body: Padding(
+          padding: const EdgeInsets.only(top:22),
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount:2,
+            childAspectRatio: 3/2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 33,),
+            itemCount: 4,
+            itemBuilder: (BuildContext context,int index){
+              return GestureDetector(
+                onTap: (){} ,
+                child: GridTile(
+                      child: Stack(
+                       children: [
+
+                        Positioned(
+                          top:-3,
+                          bottom:-9,
+                          right:0,
+                          left: 0,
+                         child: ClipRRect(
+                                       borderRadius: BorderRadius.circular(55),
+                                       child: Image.asset("img PATH")
+                                
+                           footer: GridTileBar(
+                          // backgroundColor: Color.fromARGB(66, 73, 127, 110),
+                              trailing: IconButton(
+                                color: Color.fromARGB(255, 62, 94, 70),
+                                onPressed: () { },
+                                icon: Icon(Icons.add)),
+
+                          leading: Text("\$12.99"),
+
+                          title: Text("",),
+   ), 
    ),
-
-
-            )
-          
-          
-          
-          }),
+   ],
+                       ),
+                      
+                      
+                ),
+                ),
+                        
+              )
+            
+            
+            
+            }),
+        ),
 
 
 
