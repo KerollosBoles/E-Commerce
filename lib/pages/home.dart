99214c8b90ce1788a81b2ca5d004e09c331b29a6
Kerollos Boles <boleskoko9@gmail.com>
2023-04-25 05:24:05 +0200

    importing some photos we are need it

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,6 +8,30 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
+
+        body: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount:2,
+          childAspectRatio: 3/2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 33,),
+          itemCount: 4,
+          itemBuilder: (BuildContext context,int index){
+            return GridTile(
+         
+   ),
+
+
+            )
+          
+          
+          
+          }),
+
+
+
+
+
       drawer: Drawer(
         child: Column(
          children: [
@@ -100,14 +124,15 @@ class Home extends StatelessWidget {
              
              ],
            ),
-         ],
-),
-      ),  
-Container(
+           Container(
    margin: EdgeInsets.only(bottom: 12),
    child: Text("Developed by Kerollos Boles © 2022",
  style: TextStyle(fontSize: 16)),
  )
+         ],
+),
+      ),  
+
 
 
 
